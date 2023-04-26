d98d85f507f6a7246c313a17312185f9962afcb5
Elise Flaneuse <eliseflaneuse@gmail.com>
2023-04-26 13:32:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/gherini-sound' element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<Home />}></Route>
-          <Route path="/gherini-sound/about" element={<About />} />
-          <Route path="/gherini-sound/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
